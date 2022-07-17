4324b453ea7214277249fc5ed8657f763c72ee24
acsmdev <acsmdev@gmail.com>
2022-07-17 00:03:17 -0300

    Project Created

---
+++ backend/src/main/java/br/dev/augustosoares/dsmeta/DsmetaApplication.java
@@ -0,0 +1,13 @@
+package br.dev.augustosoares.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetaApplication.class, args);
+	}
+
+}
