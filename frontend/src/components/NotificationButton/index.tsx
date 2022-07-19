d35474ace6f7b95f89188f6787d4f3011f661463
acsmdev <acsmdev@gmail.com>
2022-07-19 13:13:03 -0300

    Other Components

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,11 +1,11 @@
-import notificationButton from "../../assets/img/notification_icon.svg"
+import icon from "../../assets/img/notification_icon.svg"
 import "./style.css"
 
 function NotificationButton() {
 
     return (
         <div className="dsmeta-red-btn">
-            <img src={notificationButton} alt="notificationButton" />
+            <img src={icon} alt="notificationButton" />
         </div>
     )
 
