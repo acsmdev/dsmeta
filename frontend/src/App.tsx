0e03c333d65079015fbb526f452c08f88f4c97cf
sg23ctex <sg2.3ctex@gmail.com>
2022-07-25 12:26:25 -0300

    Security Config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/Header/Header"
-import SalesCard from "./components/SalesCard/SalesCard"
+import Header from "./components/Header/Header";
+import SalesCard from "./components/SalesCard/SalesCard";
 
-import NotificationButton from "./components/NotificationButton"
+import NotificationButton from "./components/NotificationButton";
 
 
 function App() {
